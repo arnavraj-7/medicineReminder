b143fd152843914cb760620241375cea10a1dcf3
arnavraj-7 <arnavrajcodes@gmail.com>
2025-10-08 12:37:06 +0530

    https

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.use(session({
     mongoUrl: process.env.MONGO_URI 
   }),
   cookie: {
-    secure: process.env.NODE_ENV === 'production', 
+    secure: true, 
     httpOnly: true, 
     maxAge: 1000 * 60 * 60 * 24 * 7 
   }
