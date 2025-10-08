31c16595842b30bd89d4609edc9462a6559b1154
arnavraj-7 <arnavrajcodes@gmail.com>
2025-10-08 20:12:38 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -18,10 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 6000;
 
 // --- Middleware ---
- app.use(cors({
-  origin: '*',
-  credentials: true
-}));
+app.use(cors()); 
 
 app.use(express.json());
 app.use(cookieParser());
